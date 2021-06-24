10e9fc35a6f6b8a8462e1a4edcf9da7e18730a38
Ekaterina Pavlova <tetyshkakyky@yandex.ru>
2021-06-24 17:03:45 +0300

    ex5/ex6

---
+++ count_redirects.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
+len_redirects = len(response.history)-1
+past_url = response.url
+
+print(f'Количество редиректов до итоговой точки: {len_redirects}')
+print(f'Итоговый URL: {past_url}')

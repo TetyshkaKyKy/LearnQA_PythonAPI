c3ebbd04391ff28c202662570fc8787bde9e3e8a
Ekaterina Pavlova <tetyshkakyky@yandex.ru>
2021-06-27 16:38:13 +0300

    fix count

--- Ex6_count_redirects.py
+++ Ex6_count_redirects.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-len_redirects = len(response.history)-1
+len_redirects = len(response.history)
 past_url = response.url
 
 print(f'Количество редиректов до итоговой точки: {len_redirects}')

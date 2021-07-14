94611206a2ae192288113c7ec8eaecb87f1af436
Ekaterina Pavlova <tetyshkakyky@yandex.ru>
2021-07-14 14:32:13 +0300

    framework

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,6 @@
 from requests import Response
 import json.decoder
+from datetime import datetime
 
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
@@ -20,3 +21,16 @@ class BaseCase:
 
         return response_as_dict[name]
 
+    def prepare_registration_data(self, email = None):
+        if email is None:
+            base_part = 'learnqa'
+            domain = 'example.com'
+            random_part = datetime.now().strftime('%m%d%Y%H%M%S')
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
